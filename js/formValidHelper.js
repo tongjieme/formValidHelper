08e25776e035a3c33381beb1c5e4684eb3c066c1
tongjieme <tongjieme@gmail.com>
2015-10-30 17:46:33 +0800

    更新文档

--- js/formValidHelper.js
+++ js/formValidHelper.js
@@ -9,7 +9,7 @@
 	    return string.charAt(0).toUpperCase() + string.slice(1);
 	}
     var reg = {
-			email: /^[a-zA-Z0-9_]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/,
+			email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
 			chinese: /^[\u0391-\uFFE5]+$/,
 			zipcode: /^[1-9]\d{5}$/,
 			mobile: /^1[3-9][0-9]{9}$/,
@@ -21,7 +21,11 @@
 			numbers_abc_underline: /^\w+$/,
 			url: /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
 			username: /^(?!_)(?!.*?_$)[a-zA-Z0-9_\u4e00-\u9fa5]+$/,
-			price: /^\d+(\.\d+)?$/
+			price: /^\d+(\.\d+)?$/,
+			chinaIdLoose: /^(\d{18}|\d{15}|\d{17}[xX])$/,
+			chinaZip: /^\d{6}$/,
+			ipv4: /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
+			url: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
 		},
 		msg = {
 			noChinese: '* Chinese character is not allowed.',
@@ -243,13 +247,16 @@ if($.fn.tooltipster !== undefined) {
 				    theme: 'tooltipster-error',
 				    maxWidth: 300,
 				    contentAsHTML: true,
-				    content: msg,
+				    content: msg || 'This field is invalid.',
 				    hideOnClick: true,
 				    trigger: 'custom',
 				    autoClose: true,
 				    timer: 3000,
 				    interactive: true,
-				    debug: false
+				    debug: false,
+				    functionAfter: function(){
+
+				    }
 				});
 			};
 
@@ -280,6 +287,9 @@ if($.fn.tooltipster !== undefined) {
 				isPassed: true,
 				list: []
 			};
+			if(this.showOneMessage === true) {
+				$els = $els.eq(0);
+			}
 			$.each($els, function(k,v){
 				var r = test($(v));
 
@@ -288,11 +298,19 @@ if($.fn.tooltipster !== undefined) {
 					result.list.push({
 						type: r.type,
 						$el: $(v),
-						isPending: r.isPending,
 						msg: r.msg
 					});	
 				}
 			});
+
+			if(this.autoPositionUpdate === true && result.list.length > 0) {
+				var $el = result.list[0].$el;
+				$el.trigger('focus');
+				tooltips.error($el, result.list[0].msg);
+				// $('html, body').animate({
+				// 	scrollTop: $el.offset().top
+				// });
+			}
 			return result;
 		};
 
